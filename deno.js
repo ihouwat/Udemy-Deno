153a042ae14cc80114af68a31d60cd131bc6eb09
Igor <houwat@gmail.com>
2021-11-01 17:08:56 -0400

    explore Deno metrics and how Deno JS and Deno Rust communicate

--- deno.js
+++ deno.js
@@ -4,3 +4,15 @@ const parent = Deno.args[1];
 
 if(food === 'love' && parent === 'Igor') console.log('Deno is born!');
 else console.log('Deno is still an egg!');
+
+setTimeout(() => {
+  console.log('check set timeout')
+  // Deno.metrics tell us about the communication between JS and Rust
+  console.log(Deno.metrics().opsCompletedAsync);
+}, 1000);
+
+/* 
+  Window object is part of Deno.
+  Deno strives for a 'browser-compatible' API. For example, it includes a 'fetch' API, unlike Node.js.
+*/
+console.log(window);
